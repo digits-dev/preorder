65b9c319a9cc7e356a44874d60b2d437c6e1a0bd
Mike Rodelas <mikerodelas@digits.ph>
2024-07-06 00:14:23 +0800

    add hooks after login, reject users login if inactive

--- app/Http/Controllers/CBHook.php
+++ app/Http/Controllers/CBHook.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 namespace App\Http\Controllers;
 
 use DB;
+use Illuminate\Support\Facades\Hash;
 use Session;
 use Request;
 
@@ -14,6 +15,13 @@ class CBHook extends Controller {
 	|
 	*/
 	public function afterLogin() {
-		
+		$users = DB::table(config('crudbooster.USER_TABLE'))->where("email", request('email'))->first();
+
+        if (Hash::check(request('password'), $users->password)){
+            if($users->status == "INACTIVE"){
+                Session::flush();
+                return redirect()->route('getLogin')->with('message', "Users doesn\'t exists!");
+            }
+        }
 	}
 }
