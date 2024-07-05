cc6a744438ac036f401a53163696d28f2da1a745
Mike Rodelas <mikerodelas@digits.ph>
2024-07-06 00:14:33 +0800

    clean up routes

--- routes/web.php
+++ routes/web.php
@@ -19,42 +19,55 @@ use App\Http\Controllers\AdminCustomersController;
 
 Route::get('/', function () {
     return redirect('admin/login');
-    // return view('welcome');
 });
 
-Route::post('/admin/items/inventory-upload',[AdminItemsController::class, 'inventoryUpload'])->name('item-inventory.upload');
-Route::get('/admin/items/inventory-template',[AdminItemsController::class, 'inventoryTemplate'])->name('item-inventory.template');
-Route::get('/admin/items/inventory',[AdminItemsController::class, 'inventoryView'])->name('item-inventory.view');
+Route::group(['middleware' => ['web'], 'prefix' => config('crudbooster.ADMIN_PATH'), 'namespace' => 'App\Http\Controllers'], function(){
 
-Route::post('/admin/items/upload',[AdminItemsController::class, 'itemUpload'])->name('item.upload');
-Route::get('/admin/items/template',[AdminItemsController::class, 'itemTemplate'])->name('item.template');
-Route::get('/admin/items/upload',[AdminItemsController::class, 'itemView'])->name('item.view');
+    Route::group(['prefix' => 'items'], function () {
+        Route::post('inventory-upload',[AdminItemsController::class, 'inventoryUpload'])->name('item-inventory.upload');
+        Route::get('inventory-template',[AdminItemsController::class, 'inventoryTemplate'])->name('item-inventory.template');
+        Route::get('inventory',[AdminItemsController::class, 'inventoryView'])->name('item-inventory.view');
+        Route::post('upload',[AdminItemsController::class, 'itemUpload'])->name('item.upload');
+        Route::get('template',[AdminItemsController::class, 'itemTemplate'])->name('item.template');
+        Route::get('upload',[AdminItemsController::class, 'itemView'])->name('item.view');
+        Route::post('get-models',[AdminItemsController::class, 'getItemModels'])->name('item.getItemModels');
+        Route::post('get-colors',[AdminItemsController::class, 'getItemColors'])->name('item.getItemColors');
+        Route::post('get-sizes',[AdminItemsController::class, 'getItemSizes'])->name('item.getItemSizes');
+        Route::get('delete-all-item',[AdminItemsController::class, 'itemDelete'])->name('item.delete-all');
+        Route::post('export',[AdminItemsController::class,'itemExport'])->name('item.export');
+    });
 
-Route::post('/admin/items/get-models',[AdminItemsController::class, 'getItemModels'])->name('item.getItemModels');
-Route::post('/admin/items/get-colors',[AdminItemsController::class, 'getItemColors'])->name('item.getItemColors');
-Route::post('/admin/items/get-sizes',[AdminItemsController::class, 'getItemSizes'])->name('item.getItemSizes');
+    Route::group(['prefix' => 'users'], function () {
+        Route::post('users-upload',[AdminCmsUsersController::class, 'usersUpload'])->name('users.upload');
+        Route::get('users-template',[AdminCmsUsersController::class, 'usersTemplate'])->name('users.template');
+        Route::get('users',[AdminCmsUsersController::class, 'usersView'])->name('users.view');
+    });
 
-Route::get('/admin/items/delete-all-item',[AdminItemsController::class, 'itemDelete'])->name('item.delete-all');
-Route::post('/admin/items/export',[AdminItemsController::class,'itemExport'])->name('item.export');
+    Route::group(['prefix' => 'stores'], function () {
+        Route::post('store-upload',[AdminStoresController::class, 'storeUpload'])->name('store.upload');
+        Route::get('store-template',[AdminStoresController::class, 'storeTemplate'])->name('store.template');
+        Route::get('import',[AdminStoresController::class, 'storeView'])->name('store.view');
+    });
 
-Route::post('/admin/users/users-upload',[AdminCmsUsersController::class, 'usersUpload'])->name('users.upload');
-Route::get('/admin/users/users-template',[AdminCmsUsersController::class, 'usersTemplate'])->name('users.template');
-Route::get('/admin/users/users',[AdminCmsUsersController::class, 'usersView'])->name('users.view');
+    Route::group(['prefix' => 'orders'], function () {
+        Route::post('preorder',[AdminOrdersController::class, 'preOrderSave'])->name('preorder.order');
+        Route::post('preorder-edit',[AdminOrdersController::class, 'preOrderEditSave'])->name('preorder.order-edit');
+        Route::get('preorder-cancel/{id}',[AdminOrdersController::class, 'preOrderCancel'])->name('preorder.cancel-order');
+        Route::post('item-search',[AdminItemsController::class, 'itemSearch'])->name('preorder.item-search');
+        Route::post('freebies-search',[AdminItemsController::class, 'freebiesSearch'])->name('preorder.freebies-search');
+        Route::post('item-reservable',[AdminItemsController::class, 'itemReservable'])->name('preorder.item-reservable');
+        Route::post('export',[AdminOrdersController::class,'preOrderExport'])->name('preorder.export');
+        Route::post('get-customer-order-count',[AdminOrdersController::class,'getCustomerOrderCount'])->name('preorder.getCustomerOrders');
+    });
 
-Route::post('/admin/stores/store-upload',[AdminStoresController::class, 'storeUpload'])->name('store.upload');
-Route::get('/admin/stores/store-template',[AdminStoresController::class, 'storeTemplate'])->name('store.template');
-Route::get('/admin/stores/import',[AdminStoresController::class, 'storeView'])->name('store.view');
+    Route::post('customer/get-customer',[AdminCustomersController::class,'getCustomerDetails'])->name('preorder.getCustomer');
+});
 
-Route::post('/admin/orders/preorder',[AdminOrdersController::class, 'preOrderSave'])->name('preorder.order');
-Route::post('/admin/orders/preorder-edit',[AdminOrdersController::class, 'preOrderEditSave'])->name('preorder.order-edit');
-Route::get('/admin/orders/preorder-cancel/{id}',[AdminOrdersController::class, 'preOrderCancel'])->name('preorder.cancel-order');
 
-Route::post('/admin/orders/item-search',[AdminItemsController::class, 'itemSearch'])->name('preorder.item-search');
-Route::post('/admin/orders/freebies-search',[AdminItemsController::class, 'freebiesSearch'])->name('preorder.freebies-search');
-Route::post('/admin/orders/item-reservable',[AdminItemsController::class, 'itemReservable'])->name('preorder.item-reservable');
 
-Route::post('/admin/orders/export',[AdminOrdersController::class,'preOrderExport'])->name('preorder.export');
-Route::post('/admin/customer/get-customer',[AdminCustomersController::class,'getCustomerDetails'])->name('preorder.getCustomer');
-Route::post('/admin/orders/get-customer-order-count',[AdminOrdersController::class,'getCustomerOrderCount'])->name('preorder.getCustomerOrders');
+
+
+
+
 
 
